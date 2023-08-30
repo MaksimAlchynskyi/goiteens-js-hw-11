5cc37579b4c042f31049d465342cca73fdccd86d
unknown <zloymax31@gmail.com>
2023-08-30 17:06:04 +0200

    finished

--- index.js
+++ index.js
@@ -4,28 +4,27 @@
 // Напиши функцію logItems(array), яка отримує масив і використовує цикл for, який для кожного елемента масиву буде виводити в консоль повідомлення в форматі [номер елемента] - [значення елемента].
 // Нумерація повинна починатися з 1. Наприкндлад, для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з іексо0 бум де виведено '1 - Mango', а для індексу 2 виведе '3 - Ajax'
 
-// function logItems(array) {
-//     for (let i = 0; i < array.length; i++) {
-//         const element = array[i];
-//         const elementNumber = i + 1;
-//         console.log(`${elementNumber}=> ${element}`);
-//     }
-// }
-// logItems(['Mango', 'Poly', 'Ajax','Mango', 'Poly', 'Ajax']);
+function logItems(array) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const elementNumber = i + 1;
+        console.log(`${elementNumber}=> ${element}`);
+    }
+}
+logItems(['Mango', 'Poly', 'Ajax','Mango', 'Poly', 'Ajax']);
 
 // Завдання 2
 // Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і
 //  ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
-// let price = 0;
-// function calculateEngravingPrice(message, pricePerWord) {
-//    const elementArray = message.split(" ")
-//    price = elementArray.length * pricePerWord;
-//    return price
-// }
-// // calculateEngravingPrice()
-// console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20));
-// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40));
-// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20));
+let price = 0;
+function calculateEngravingPrice(message, pricePerWord) {
+   const elementArray = message.split(" ");
+   price = elementArray.length * pricePerWord;
+   return price
+}
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20));
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40));
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20));
 
 // Завдання 3
 // Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок 
@@ -47,3 +46,12 @@ console.log(findLongestWord('May the force be with you'));
 // Завдання 4
 // Напиши функцію formatString(string) яка приймає рядок і форматує його якщо необхідно.
 
+function formatString(string) { 
+    if (string.length < 40) {
+return string
+    } else {
+     return string.slice(0, 40) + "..."
+      
+    }
+}
+console.log(formatString('ак от, усі людські пристрасті , розпалені зіткненнями інтересів у вашому нинішньому суспільстві, проходять переді мною, і я влаштовую їм огляд, а сам живу спокійно.'));
